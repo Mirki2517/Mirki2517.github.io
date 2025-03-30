d9a62da6339ca3d77ca16ec8ecf4725acc7fd6cf
Mirki2517 <miquircomegalletas@gmail.com>
2025-03-30 14:12:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+window.sr = ScrollReveal ();
+
+sr.reveal('.senn-arriba',{
+
+duration: 3000,
+origin: 'rigth',
+distance: '-200px'
+})
+
+sr.reveal('.senn-abajo',{
+
+    duration: 3000,
+    origin: 'left',
+    distance: '-500px'
+    })
+    
+
+
+
+sr.reveal('.laboral',{
+
+    duration: 3000,
+    origin: 'top',
+    distance: '-300px'
+    })
+    
+
+    sr.reveal('.areas',{
+
+        duration: 3000,
+        origin: 'right',
+        distance: '-400px'
+        })
+
+
+        sr.reveal('.redes-sena',{
+
+            duration: 3000,
+            origin: 'top',
+            distance: '-500px'
+            })
+
+            sr.reveal('.derechos',{
+
+                duration: 3000,
+                origin: 'top',
+                distance: '-500px'
+                })
+   
+                sr.reveal('.nuestras-redes',{
+
+                    duration: 3000,
+                    origin: 'top',
+                    distance: '-500px'
+                    })
+       
